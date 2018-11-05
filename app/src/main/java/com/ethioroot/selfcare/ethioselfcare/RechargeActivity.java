dd478460539bc2c62747de3a9f23a8e79a83162b
shadyber <shady.ber9@gmail.com>
2018-11-05 20:02:33 +0300

    OCR recharege is added

--- app/src/main/java/com/ethioroot/selfcare/ethioselfcare/RechargeActivity.java
+++ app/src/main/java/com/ethioroot/selfcare/ethioselfcare/RechargeActivity.java
@@ -1,42 +1,89 @@
 package com.ethioroot.selfcare.ethioselfcare;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
+import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class RechargeActivity extends AppCompatActivity {
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.Text;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+public class RechargeActivity extends AppCompatActivity  implements SurfaceHolder.Callback, Detector.Processor  {
 
     EditText txtcard;
     TextView txtcounter;
 
 
+    private SurfaceView cameraView;
+    private TextView txtView;
+    private CameraSource cameraSource;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (Exception ignored) {
+                        Log.i("Ignorred :",ignored.getMessage());
+                    }
+                }
+            }
+            break;
+        }
+    }
 
 
-public void RecharegCard(View view){
+    public void RecharegCard(View view){
     PhoneCaller.MakeCall("*805*"+txtcard.getText().toString()+"#",this);
 }
+
+    public void RecharegCard(String card){
+        PhoneCaller.MakeCall("*805*"+card+"#",this);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recharge);
+
+        cameraView = findViewById(R.id.surface_view);
+        txtView = findViewById(R.id.txtview);
+        TextRecognizer txtRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!txtRecognizer.isOperational()) {
+            Log.e("Main Activity", "Detector dependencies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), txtRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(this);
+            txtRecognizer.setProcessor(this);
+        }
+
 
         txtcounter = findViewById(R.id.typedDigit);
         txtcard = findViewById(R.id.txtCardNumber);
@@ -59,4 +106,115 @@ public void RecharegCard(View view){
             }
         });
     }
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},1);
+                return;
+            }
+            cameraSource.start(cameraView.getHolder());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        cameraSource.stop();
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void receiveDetections(Detector.Detections detections) {
+        SparseArray items = detections.getDetectedItems();
+        final StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < items.size(); i++)
+        {
+            TextBlock item = (TextBlock)items.valueAt(i);
+            strBuilder.append(item.getValue());
+            strBuilder.append("/");
+            // The following Process is used to show how to use lines & elements as well
+            for (int j = 0; j < items.size(); j++) {
+                TextBlock textBlock = (TextBlock) items.valueAt(j);
+                strBuilder.append(textBlock.getValue());
+                strBuilder.append("/");
+                for (Text line : textBlock.getComponents()) {
+                    //extract scanned text lines here
+                    Log.v("lines", line.getValue());
+                    strBuilder.append(line.getValue());
+                    // strBuilder.append("/");
+                    for (Text element : line.getComponents()) {
+                        //extract scanned text words here
+                        Log.v("element", element.getValue());
+
+
+                        txtView.setText(element.getValue());
+                        if(element.getValue().matches("^[0-9]*$") && element.getValue().length() ==13){
+                            final String ele=element.getValue();
+
+                            //   strBuilder.append(element.getValue());
+                            txtView.post(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(RechargeActivity.this);
+
+                                    builder.setTitle("Confirm");
+                                    builder.setMessage("Your card Number is : "+ele);
+
+                                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            // Do nothing but close the dialog
+
+                                            txtcard.setText(ele);
+                                            RecharegCard(ele);
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                                    builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                            // Do nothing
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                                    AlertDialog alert = builder.create();
+                                    alert.show();
+
+
+
+
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+            }
+        }
+        // Log.v("strBuilder.toString()", strBuilder.toString());
+
+
+    }
+
 }
